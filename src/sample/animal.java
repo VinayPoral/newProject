3c5e47663ba18dcf17f60453e3149d4050838341
VinayPoral <poralvinay1997@gmail.com>
2024-06-21 20:59:39 +0530

    Intial Commit

---
+++ src/sample/animal.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.Scanner;
+
+// added new comment by vinay -1
+public class animal {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Please enter your Palindrom :");
+		String input = scan.next();
+		
+		String temp = " ";
+		for(int i=0;i<input.length();i++) {
+			temp = input.charAt(i)+ temp;
+			
+		}
+		
+		if(temp.equalsIgnoreCase(temp)) {
+			System.out.println("The given String is palindrom");
+		}else {
+			System.out.println("The given String is not palindrom");
+		}
+	}
+	}
